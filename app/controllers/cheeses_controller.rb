18ec4d80ca3e9d2d03f4db1f88490b93e0f00175
WizardofHause <jonrhause@gmail.com>
2022-11-22 16:00:46 -0700

    commit tree 59c7bb31658a72f71bdd1f9d87d03af78bc253d6

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,13 +3,23 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, only: [:id, :name, :price, :is_best_seller]
   end
+
+  # # OR... this does the same thing with EXCEPT...
+  # def index
+  #   cheeses = Cheese.all
+  #   render json: cheeses, except: [:created_at, :updated_at]
+  # end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    if cheese
+      render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      render json: { error: 'The cheese stands alone, and you are the cheese' }, status: :not_found
+    end
   end
 
 end
